fee5914a32187bd9e5874e8826c523e1736f7c65
Marc-Ph <mcphilli8@gmail.com>
2017-01-21 09:37:01 +0100

    BLa BKa
    Fürn Mac

---
+++ index.php
@@ -0,0 +1,31 @@
+<!Doctype HTML>
+<html>
+    <head>
+        <title> Gemeinde Gottes Trossingen </title>
+        <link rel="stylesheet" href="style.css">
+        <meta charset="utf-8"/>
+
+    </head>
+
+    <body>
+        <div id="box1">
+            <ul id = "headermenu">
+                <li id ="first">Home</li>
+                <li id ="second">Livestream</li>
+                <li id ="third">Predigten</li>
+                <li id ="fourth">Spenden</li>
+                <li id ="fifth">Regelmäßige Gottesdienste</li>
+            </ul>
+        </div>
+
+        <div id="box2">
+        </div>
+
+        <div id="box3">
+        </div>
+        
+        <div id="box4">
+        </div>
+    </body>
+
+</html>
